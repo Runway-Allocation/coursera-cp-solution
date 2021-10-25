58ad01d4707f18f298149c53ae96ed77af182341
Lawliet3601 <aadirawat2001@gmail.com>
2021-10-25 15:17:45 +0530

    initial 4.0

---
+++ compare_sums.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int main(){
+
+    int n;
+    cin >> n;
+    vector<double> a(n), b(n);
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+    for (int i = 0; i < n; ++i)
+        cin >> b[i];
+
+    bool equal_sum = false;
+    bool sum_a_larger = false;
+    // your code
+
+    double eps = 1e-6;
+    double s1 = accumulate(a.begin(), a.end(), 0.0);
+    double s2 = accumulate(b.begin(), b.end(), 0.0);
+
+    if (abs(s1-s2) < eps)
+        equal_sum = true;
+    
+    if (s2 < s1 - eps)
+        sum_a_larger = true;
+
+    if (equal_sum)
+        cout << "SUM(A)=SUM(B)" << endl;
+    else {
+        if (sum_a_larger)
+            cout << "SUM(A)>SUM(B)" << endl;
+        else
+        cout << "SUM(A)<SUM(B)" << endl;
+    }
+
+    return 0;
+}
